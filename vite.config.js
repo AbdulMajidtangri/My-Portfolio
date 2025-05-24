e769469683a4e1a75596cec78ac62d544541d7fb
abdul majid <majidalitangri7@gmail.com>
2025-05-24 22:14:23 +0500

    config-setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures assets use relative paths (important on Vercel)
+  build: {
+    outDir: 'dist' // Ensures Vercel looks in the right folder
+  }
 })
