a995d28f927fdc83a9e9f18f5999143ceadaa283
abdul majid <majidalitangri7@gmail.com>
2025-06-11 22:23:42 +0500

    change-mode-functionality-added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0a192f",
+        secondary: "#64ffda",
+        tertiary: "#8892b0",
+        light: "#ccd6f6"
+      },
+    },
   },
   plugins: [],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './', // Ensures assets use relative paths (important on Vercel)
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
   build: {
     outDir: 'dist' // Ensures Vercel looks in the right folder
   }
